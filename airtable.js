9ccff72924aa11e9c5d2c27b71688bfde950e1eb
CellBody <austintingwork@gmail.com>
2018-09-20 21:29:04 +0800

    Add airtable
    Test airtable API.

--- airtable.js
+++ airtable.js
@@ -18,3 +18,20 @@ module.exports.read = async ({table, maxRecords, pageSize, sort, offset}) => {
   console.log(option)
   return rp(option)
 }
+
+// const test = async () => {
+//   const result = await airtable.read({
+//     table: 'Furniture',
+//     maxRecords: '5',
+//     pageSize: 3,
+//     sort: [{field: 'Name', direction: 'desc'}]
+//   })
+//   const {records} = result
+//   // console.log(JSON.stringify(result))
+//   console.log(result)
+//   console.log(records[0].fields.Name)
+//   console.log(records[1].fields.Name)
+//   console.log(records[2].fields.Name)
+// }
+
+// test()
