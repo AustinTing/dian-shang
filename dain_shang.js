a43e9531cf8645504d789187c52df3a061ff8ae8
CellBody <austintingwork@gmail.com>
2018-09-20 21:32:25 +0800

    Add bottender

--- dain_shang.js
+++ dain_shang.js
@@ -1,18 +1,26 @@
-const airtable = require('./airtable')
+require('dotenv').config()
 
-const test = async () => {
-  const result = await airtable.read({
-    table: 'Furniture',
-    maxRecords: '5',
-    pageSize: 3,
-    sort: [{field: 'Name', direction: 'desc'}]
-  })
-  const {records} = result
-  // console.log(JSON.stringify(result))
-  console.log(result)
-  console.log(records[0].fields.Name)
-  console.log(records[1].fields.Name)
-  console.log(records[2].fields.Name)
-}
+const { LineBot, FileSessionStore } = require('bottender')
+const { createServer } = require('bottender/express')
 
-test()
+const bot = new LineBot({
+  accessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+  sessionStore: new FileSessionStore('./sessions', 365 * 24 * 60 * 10) // 10 years
+})
+
+bot.onEvent(async context => {
+  if (context.event.isFollow) {
+    await context.sendText('Hello, welcome to this bot!')
+  } else if (context.event.isText && context.event.text === 'How are you?') {
+    await context.sendText('I am fine.')
+  } else {
+    await context.sentText('I do not understand.')
+  }
+})
+
+const server = createServer(bot)
+
+server.listen(process.env.PORT, () => {
+  console.log(`server is running on ${process.env.PORT} port...`)
+})
